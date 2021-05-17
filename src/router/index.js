071e0830ab7a6261ac1dd1a6fee6982c75c7447a
linaibao <844944333@qq.com>
2021-05-17 20:39:02 +0800

    根路由3

--- src/router/index.js
+++ src/router/index.js
@@ -48,7 +48,7 @@ const routes = [
 ]
 // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes,
   linkActiveClass:"active"

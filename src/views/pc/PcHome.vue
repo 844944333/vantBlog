586d2bdd672f07fc9614afb6eb752b9b6f0665b2
wesmiler <wesmiler@163.com>
2021-05-21 17:28:49 +0800

    desc

--- src/views/pc/PcHome.vue
+++ src/views/pc/PcHome.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div class="home">
         Home of Pc
+
+        网站是能正常访问的,请使用移动端打开网站,或者按 F12 切换移动端.
     </div>
 </template>
 
@@ -11,5 +13,9 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-
+.home {
+    padding-top: 50px;
+    text-align: center;
+    font-size: 36px;
+}
 </style>
